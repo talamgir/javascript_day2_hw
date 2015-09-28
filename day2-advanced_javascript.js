b0befe13033de0a31e64579d0ba9d30675364da8
Justin Bardliving <J@Js-MacBook-Air.local>
2015-09-28 13:36:45 -0400

    javascript_day2_hw

---
+++ day2-advanced_javascript.js
@@ -0,0 +1,56 @@
+//make an array with two arrays/loop
+
+var friends=[["katie","john","sam","crhis"],["christine","matt","nathan","harry"]]
+
+for(i=1;i<friends.length;i+=1){
+	alert(friends[0])
+}
+for (i=1;i<friends.length;i+=1){
+	alert(friends[1])
+}
+//loop for 99 bottles
+for(i=100;i>0;i--){
+	console.log( i+ " bottles of root beer on the wall...take one down pass it around")
+
+}
+
+// global and local scopes. A variable is also global if it is not declared with a var in a function.
+
+	//local					
+function hey(){
+	var hi=5
+	return hi
+}
+hey()
+
+//global
+var numbs=6
+function numbers(){
+
+}
+
+//palidrome. 
+
+
+//cheking a string for vowels
+
+
+function vowelTest(s) {
+  return (/^[aeiou]$/i).test(s);
+}
+
+//text box with my name popping up as an alert
+in html 
+
+//most frequency in a array. 
+
+
+
+
+
+
+
+
+
+
+
